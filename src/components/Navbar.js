dae533c1fdd57a5eea08ee486a6b3fc2acf9dc1e
yosuke026 <y3kh6s03@gmail.com>
2023-07-24 09:51:04 +0900

    add-logout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,24 @@ import "./Navbar.scss";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouse, faFilePen, faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons'
 
-const Navbar = () => {
+const Navbar = ({ isAuth }) => {
     return (
         <nav>
-            <Link to="/"><FontAwesomeIcon icon={faHouse} />ホーム</Link>
-            <Link to="/CreatePost"><FontAwesomeIcon icon={faFilePen} />新規投稿</Link>
-            <Link to="/login"><FontAwesomeIcon icon={faArrowRightToBracket} />ログイン</Link>
+            <Link to="/">
+                <FontAwesomeIcon icon={faHouse} />ホーム
+            </Link>
+            <Link to="/CreatePost">
+                <FontAwesomeIcon icon={faFilePen} />新規投稿
+            </Link>
+            {!isAuth
+                ? <Link to="/login">
+                    <FontAwesomeIcon icon={faArrowRightToBracket} />ログイン
+                </Link>
+                : <Link to="/logout">
+                    <FontAwesomeIcon icon={faArrowRightToBracket} />ログアウト
+                </Link>
+            }
+
         </nav>
     )
 }
